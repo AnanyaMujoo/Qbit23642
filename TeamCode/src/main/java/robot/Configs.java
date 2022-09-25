46fdff68ba3374f765159d29e446f8fe8cb3e78d
shuhul <shuhulmujoo@gmail.com>
2022-09-24 18:14:08 -0700

    v4.0.5 - Problem with Camera (NullPointerException if added to config) - WARNING: Problem with this version

--- TeamCode/src/main/java/robot/Configs.java
+++ TeamCode/src/main/java/robot/Configs.java
@@ -7,24 +7,24 @@ public class Configs implements RobotUser{
     /**
      * Used to test only the test part
      */
-    public static RobotConfig TestConfig = new RobotConfig(customTestPart);
+    static RobotConfig TestConfig = new RobotConfig(customTestPart);
 
     /**
      * Used for tank robots
      */
-    public static RobotConfig TankConfig = new RobotConfig(tankDrive, tankIntake, tankTurret, tankLift, tankOuttake, tankCarousel, color, gyro, odometry, camera);
+    static RobotConfig TankConfig = new RobotConfig(tankDrive, tankIntake, tankTurret, tankLift, tankOuttake, tankCarousel, color, gyro, odometry, camera);
 
     /**
      * Used for mecanum robots
      */
-    public static RobotConfig MecanumConfig = new RobotConfig(drive, gyro, odometry, camera, carousel, intake, lift, outtake, color);
-
+//    static RobotConfig MecanumConfig = new RobotConfig(drive, gyro, odometry, camera, carousel, intake, lift, outtake, color);
+    static RobotConfig MecanumConfig = new RobotConfig(drive, gyro, odometry, carousel, intake, lift, outtake, color);
 
     /**
      * Current Config
      */
     public static void setCurrentConfig(){
-        setConfig(TestConfig);
+        setConfig(MecanumConfig);
     }
 
 }
